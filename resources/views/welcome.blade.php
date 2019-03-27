2a99b99ac162d007f6ddc8c41683125be02188d7
dauthang81197 <thangdau811@gmail.com>
2019-03-27 21:46:38 +0700

    file commit

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,7 @@
+<html>
+	<head></head>
+	<body>
+		<h1>docker la mot nen tang mo giup nguoi su dung khoi tao phat trien va khoi chay ung dung</h1>
+		<h2>container giong nhu 1 thu muc, cung cap nhung thu can thiet de de mot ung dung co the chay duoc</h2>
+	</body>
+</html>
